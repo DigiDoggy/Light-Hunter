94e248e76f738a0be2883238943de1e8005768f0
Ilja Aituganov <aituganov.ilja@gmail.com>
2025-08-10 21:20:35 +0300

    check

--- frontend/src/js/Camera.js
+++ frontend/src/js/Camera.js
@@ -4,12 +4,19 @@ export default class Camera {
         this.gameContainer = gameContainer ? gameContainer : document.getElementById("gameContainer");
     }
 
-    updateCamera(x, y, width, height) {
-        const centerX = x + (width / 2);
-        const centerY = y + (height / 2);
 
-        const translateX = (window.innerWidth / 2) - (centerX * this.zoomLevel);
-        const translateY = (window.innerHeight / 1.5) - (centerY * this.zoomLevel);
+    updateCamera(x, y, width, height) {
+        const playerCenterX = x + width / 2;
+        const playerCenterY = y + height / 2;
+
+        const viewSize = 3000;
+        this.zoomLevel = viewSize / Math.max(2000, 2000);
+
+        const screenW = window.innerWidth;
+        const screenH = window.innerHeight;
+
+        const translateX = (screenW / 2) - (playerCenterX*this.zoomLevel - 500);
+        const translateY = (screenH / 2) - (playerCenterY * this.zoomLevel - 500);
 
         this.gameContainer.style.transform = `translate(${translateX}px, ${translateY}px) scale(${this.zoomLevel})`;
     }
